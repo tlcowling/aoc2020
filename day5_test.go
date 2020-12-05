9461ed3eb10a8e1a53d116e1409550173071c729
Tom Cowling <tcow@fastmail.com>
2020-12-05 06:50:24 +0100

    remove duplicate function

--- day5_test.go
+++ day5_test.go
@@ -12,19 +12,19 @@ func TestDay5(t *testing.T) {
 	seatMap := make(map[int]bool)
 
 	max := 0
-	for _, line:=range lines {
+	for _, line := range lines {
 		rowMin := 0
 		rowMax := 127
 		colMin := 0
 		colMax := 7
 		tokens := strings.Split(line, "")
-		for _, rowData := range tokens[0:len(tokens)-3] {
-			rowMin, rowMax = findRow(rowData, rowMin, rowMax)
+		for _, rowData := range tokens[0 : len(tokens)-3] {
+			rowMin, rowMax = findPlace(rowData, rowMin, rowMax)
 		}
 		for _, colData := range tokens[len(tokens)-3:] {
-			colMin, colMax = findCol(colData,colMin, colMax)
+			colMin, colMax = findPlace(colData, colMin, colMax)
 		}
-		id:=rowMin*8+ colMin
+		id := calculateId(rowMin, colMin)
 		if id > max {
 			max = id
 		}
@@ -33,14 +33,14 @@ func TestDay5(t *testing.T) {
 
 	t.Log(max)
 
-	for i:=0; i<=127; i++ {
-		for j:=0; j<=7; j++ {
-			previousSeat := seatMap[calculateId(i-1,j-1)]
-			currentSeat := seatMap[calculateId(i,j)]
-			nextSeat := seatMap[calculateId(i+1,j+1)]
+	for i := 0; i <= 127; i++ {
+		for j := 0; j <= 7; j++ {
+			previousSeat := seatMap[calculateId(i-1, j-1)]
+			currentSeat := seatMap[calculateId(i, j)]
+			nextSeat := seatMap[calculateId(i+1, j+1)]
 			if previousSeat && nextSeat {
 				if !currentSeat {
-					t.Log(i,j,calculateId(i,j))
+					t.Log(i, j, calculateId(i, j))
 				}
 			}
 		}
@@ -51,16 +51,9 @@ func calculateId(row, col int) int {
 	return row*8 + col
 }
 
-func findRow(place string, min, max int) (int, int) {
-	if place == "F" {
-		return min, (max+min)/2
+func findPlace(place string, min, max int) (int, int) {
+	if place == "F" || place == "L" {
+		return min, (max + min) / 2
 	}
-	return ((max+min)/2)+1, max
-}
-
-func findCol(place string, min, max int) (int, int) {
-	if place == "L" {
-		return min, (max+min)/2
-	}
-	return ((max+min)/2)+1, max
+	return ((max + min) / 2) + 1, max
 }
