d72ac0d343bd89d52732ebf0cf4a300d3e54660a
Tom Cowling <tcow@fastmail.com>
2020-12-09 10:04:11 +0100

    add d9

---
+++ day9_test.go
@@ -0,0 +1,70 @@
+package aoc2020
+
+import (
+	"github.com/tlcowling/adventutils"
+	"testing"
+)
+
+func TestDay9(t *testing.T) {
+	lines := adventutils.ReadInputAsInts("./inputs/day9.txt")
+	preambleSize := 25
+	preamble := lines[0:preambleSize]
+	rest := lines[preambleSize:]
+	lastValid := []int{}
+
+	for i := 0; i < preambleSize; i++ {
+		lastValid = append(lastValid, preamble[i])
+	}
+
+	var firstInvalidNumber int
+	for _, p := range rest {
+		if !arrayContainsSumToN(lastValid, p) {
+			firstInvalidNumber = p
+			t.Log(firstInvalidNumber)
+			break
+		}
+		lastValid = append(lastValid[1:], p)
+	}
+
+	target := firstInvalidNumber
+	for i := 0; i < len(lines)-1; i++ {
+		sumNums := []int{lines[i]}
+		sumFromI := lines[i]
+		for j := i + 1; j < len(lines); j++ {
+			sumFromI += lines[j]
+			sumNums = append(sumNums, lines[j])
+			if sumFromI == target {
+				min, max := arrayMinMax(sumNums)
+				t.Log(min + max)
+				return
+			}
+		}
+	}
+}
+func arrayContainsSumToN(arr []int, n int) bool {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if i == j {
+				continue
+			}
+			if arr[i]+arr[j] == n {
+				return true
+			}
+		}
+	}
+	return false
+}
+func arrayMinMax(arr []int) (int, int) {
+	min := arr[0]
+	max := arr[0]
+	for i := 0; i < len(arr); i++ {
+		el := arr[i]
+		if el < min {
+			min = el
+		}
+		if el >= max {
+			max = el
+		}
+	}
+	return min, max
+}
